90c825ba175cc1ad240964c4b2b16d8c7b463c15
razing337 <151864678+razing337@users.noreply.github.com>
2024-03-04 01:28:47 +0300

    Update player.py

--- player.py
+++ player.py
@@ -8,60 +8,65 @@ class Player:
         self.player_size = 10
         self.rect = pygame.Rect(self.x, self.y, self.player_size, self.player_size)
         self.color = (250, 120, 60)
-        self.velX = 0
-        self.velY = 0
-        self.left_pressed = False
-        self.right_pressed = False
-        self.up_pressed = False
-        self.down_pressed = False
         self.speed = 4
 
-    # текущее местоположение
-    def get_current_cell(self, x, y, grid_cells):
+    # метод для обновления позиции
+    def update_position(self, keys_pressed, grid_cells, thickness, tile):
+        self._handle_input(keys_pressed)
+        self._move(grid_cells, thickness, tile)
+
+    # метод для обработки ввода пользователя
+    def _handle_input(self, keys_pressed):
+        if keys_pressed[pygame.K_LEFT]:
+            self.x -= self.speed
+        if keys_pressed[pygame.K_RIGHT]:
+            self.x += self.speed
+        if keys_pressed[pygame.K_UP]:
+            self.y -= self.speed
+        if keys_pressed[pygame.K_DOWN]:
+            self.y += self.speed
+
+    # метод для перемещения игрока и проверки столкновений с стенами
+    def _move(self, grid_cells, thickness, tile):
+        current_cell_x, current_cell_y = self.x // tile, self.y // tile
+        current_cell = self._get_current_cell(current_cell_x, current_cell_y, grid_cells)
+        current_cell_abs_x, current_cell_abs_y = current_cell_x * tile, current_cell_y * tile
+
+        self._check_wall_collision(current_cell, thickness, tile, current_cell_abs_x, current_cell_abs_y)
+
+        self.rect = pygame.Rect(int(self.x), int(self.y), self.player_size, self.player_size)
+
+    # метод для получения текущей ячейки игрока
+    def _get_current_cell(self, x, y, grid_cells):
         for cell in grid_cells:
             if cell.x == x and cell.y == y:
                 return cell
 
-    # нельзя проходить сквозь стены
-    def check_move(self, tile, grid_cells, thickness):
-        current_cell_x, current_cell_y = self.x // tile, self.y // tile
-        current_cell = self.get_current_cell(current_cell_x, current_cell_y, grid_cells)
-        current_cell_abs_x, current_cell_abs_y = current_cell_x * tile, current_cell_y * tile
-        if self.left_pressed:
-            if current_cell.walls['left']:
-                if self.x <= current_cell_abs_x + thickness:
-                    self.left_pressed = False
-        if self.right_pressed:
-            if current_cell.walls['right']:
-                if self.x >= current_cell_abs_x + tile - (self.player_size + thickness):
-                    self.right_pressed = False
-        if self.up_pressed:
-            if current_cell.walls['top']:
-                if self.y <= current_cell_abs_y + thickness:
-                    self.up_pressed = False
-        if self.down_pressed:
-            if current_cell.walls['bottom']:
-                if self.y >= current_cell_abs_y + tile - (self.player_size + thickness):
-                    self.down_pressed = False
+    # метод для проверки столкновений с стенами
+    def _check_wall_collision(self, current_cell, thickness, tile, current_cell_abs_x, current_cell_abs_y):
+        if self.x <= current_cell_abs_x + thickness:
+            self.x = current_cell_abs_x + thickness
+        if self.x >= current_cell_abs_x + tile - (self.player_size + thickness):
+            self.x = current_cell_abs_x + tile - (self.player_size + thickness)
+        if self.y <= current_cell_abs_y + thickness:
+            self.y = current_cell_abs_y + thickness
+        if self.y >= current_cell_abs_y + tile - (self.player_size + thickness):
+            self.y = current_cell_abs_y + tile - (self.player_size + thickness)
 
-    # иконка игрока
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
-
-    # обновление позиции
-    def update(self):
-        self.velX = 0
-        self.velY = 0
-        if self.left_pressed and not self.right_pressed:
-            self.velX = -self.speed
-        if self.right_pressed and not self.left_pressed:
-            self.velX = self.speed
-        if self.up_pressed and not self.down_pressed:
-            self.velY = -self.speed
-        if self.down_pressed and not self.up_pressed:
-            self.velY = self.speed
-
-        self.x += self.velX
-        self.y += self.velY
-
-        self.rect = pygame.Rect(int(self.x), int(self.y), self.player_size, self.player_size)
+    # модель игрока
+    def hero(self):
+        # ...
+        self.running_images = [pygame.image.load('\img\run1.png'),
+                               pygame.image.load('\img\run2.png'),
+                               pygame.image.load('\img\run3.png'),
+                               pygame.image.load('\img\run4.png'),
+                               pygame.image.load('\img\run5.png'),
+                               pygame.image.load('\img\run6.png'),
+                               pygame.image.load('\img\run7.png'),
+                               pygame.image.load('\img\run8.png'),
+                               pygame.image.load('\img\run9.png'),
+                               pygame.image.load('\img\run10.png'),]
+        self.standing_image = pygame.image.load('\img\stand.png')
+        self.current_image = self.standing_image
+        self.current_frame = 0
+        self.is_running = False
