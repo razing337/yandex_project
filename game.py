bc15a1ebee532606135776f69c6e0341f61eb942
razing337 <151864678+razing337@users.noreply.github.com>
2024-03-04 01:30:56 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame
 
 pygame.font.init()
@@ -13,15 +15,25 @@ class Game:
     # добавление точки финиша
     def add_goal_point(self, screen):
         # картинка у финиша
-        img_path = "/img/gate.png"
+        img_path = "\img\gate.png"
         img = pygame.image.load(img_path)
         img = pygame.transform.scale(img, (self.tile, self.tile))
         screen.blit(img, (self.goal_cell.x * self.tile, self.goal_cell.y * self.tile))
 
     # победное сообщение
-    def message(self):
-        msg = self.font.render('You Win!!', True, self.message_color)
-        return msg
+    def show_win_window(self):
+        win_img_path = os.path.join("\img\win.jpg")
+        win_img = pygame.image.load(win_img_path)
+        win_img = pygame.transform.scale(win_img, (600, 600))
+        win_screen = pygame.display.set_mode((600, 600))
+        win_screen.blit(win_img, (0, 0))
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+        pygame.quit()
 
     # дошел ли игрок до финиша
     def is_game_over(self, player):
